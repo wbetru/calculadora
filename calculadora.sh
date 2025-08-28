756612c55983778375fc1ffa25ee73cf4b3f03ea
wbetru <wilfernandes222@hotmail.com>
2025-08-28 15:18:44 -0300

    Update calculadora.sh

--- calculadora.sh
+++ calculadora.sh
@@ -7,31 +7,31 @@ echo "3. Multiplicação"
 echo "4. Divisão"
 
 read -p "Digite sua escolha (1/2/3/4): " escolha
-read -p "Digite o primeiro número: "  num1
+read -p "Digite o primeiro número: " num1
 read -p "Digite o segundo número: " num2
 
 case $escolha in
 1)
- resultado=$(echo "$num1 + $num2" | bc)
- echo "Resultado: $resultado"
-;;
+  resultado=$(echo "$num1 + $num2" | bc)
+  echo "Resultado: $resultado"
+  ;;
 2)
- resultado=$(echo "$num1 - $num2" | bc)
- echo "Resultado: $resultado"
-;;
+  resultado=$(echo "$num1 - $num2" | bc)
+  echo "Resultado: $resultado"
+  ;;
 3)
- resultado=$(echo "$num1 * $num2" | bc)
- echo "Resultado: $resultado"
-;;
+  resultado=$(echo "$num1 * $num2" | bc)
+  echo "Resultado: $resultado"
+  ;;
 4)
- if [$num2 -ne 0 ]; then
- resultado=$(echo "scale=2; $num1 / $num2" | bc)
- echo "Resultado: $resultado"
- else
- echo "Erro: Divisão por zero"
- fi
- ;;
- *)
- echo "Escolha inválida"
- ;;
+  if [ $num2 -ne 0 ]; then
+    resultado=$(echo "scale=2; $num1 / $num2" | bc)
+    echo "Resultado: $resultado"
+  else
+    echo "Erro: Divisão por zero"
+  fi
+  ;;
+*)
+  echo "Escolha inválida"
+  ;;
 esac
